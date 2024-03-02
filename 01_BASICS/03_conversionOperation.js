e6fec5bfa9cee019df7d0077417fc615491b4f52
Umesh Maurya <mauryau103@gmail.com>
2024-03-02 17:27:44 +0000

    conversion of datatype

---
+++ 01_BASICS/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = 33
+// console.log(typeof score)
+
+let stringNumber = "Umesh"
+
+let valueInNumber = Number(stringNumber)
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN (not a number)
+//null => 0
+//true => 1, false => 0
+//undefined => NaN
+//"umesh" => NaN
+
+let isLoggedIn = "umesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1 => true; 0 => false
+//"" => false ; "umesh" => true
+
+let someNumber = 33
+
+let strNumber = String(someNumber)
+console.log(someNumber);
+console.log(typeof strNumber);
