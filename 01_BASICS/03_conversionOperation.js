2a6ca527a4f5cd26e24b0e40f797320f35463a5e
Umesh Maurya <mauryau103@gmail.com>
2024-03-02 17:43:34 +0000

    conversion

--- 01_BASICS/03_conversionOperation.js
+++ 01_BASICS/03_conversionOperation.js
@@ -24,5 +24,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let strNumber = String(someNumber)
-console.log(someNumber);
-console.log(typeof strNumber);
+// console.log(someNumber);
+// console.log(typeof strNumber);
+
+//**********************Operations*****************************
+
+let value = 3
+let negValue = -value
+/*
+console.log(negValue);
+
+console.log(2+2); //4
+console.log(2-2); //0
+console.log(2*2); //4
+console.log(2**3); //8
+console.log(2/3); //0.6666 
+console.log(2%3); //2
+console.log(0/3); //0
+console.log(3/0); //Infinity
+*/
+
+let str1 = "hello"
+let str2 = " Umesh"
+
+console.log(str1 + str2); //hello Umesh
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 +2); //122
+console.log(1 + 2 + "2"); //32 
+
+console.log(+true); //1
+console.log(+""); //0
